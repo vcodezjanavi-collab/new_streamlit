74de24a1c01df208fdee60e1d6ebfac62e7066e8
vcodezjanavi-collab <vcodez.janavi@gmail.com>
2026-01-07 18:20:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome DS Interns",
+    page_icon="🤖",
+    layout="centered"
+)
+
+st.title("Hi Janavi")
+st.subheader("Welcome my dear DS Interns 🚀")
+st.subheader("Learn Streamlit")
+st.subheader("Python Framework")
